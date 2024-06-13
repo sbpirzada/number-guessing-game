c7f3b9acedec658c6acd00a3eae5305bda53997a
Shah Bano Pirzada <159937919+sbpirzada@users.noreply.github.com>
2024-06-13 21:51:56 +0500

    First push to number guessing game.

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/env code
+import inquirer from "inquirer";
+console.log("\n\tWelcome to Shah Bano Pirzada - CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 9 + 1);
+const answer = await inquirer.prompt([{
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guessed number (Number Limit 1 to 9):",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! You guessed a correct number.");
+}
+else {
+    console.log("Sorry! You guessed a incorrect number");
+}
